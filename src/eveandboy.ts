7aed13c2ee6287d3291fe366a402af1c2f32bf26
kongdev <nopaket_kong@hotmail.com>
2022-10-15 13:11:48 +0700

    ok eve and boy

--- src/eveandboy.ts
+++ src/eveandboy.ts
@@ -6,86 +6,85 @@ import { format } from '@fast-csv/format'
 import { scrollPageToBottom } from 'puppeteer-autoscroll-down'
 
 export const run = async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-sandbox'],
+    })
+    const page = await browser.newPage()
+    await page.setViewport({ width: 0, height: 0 })
+
+    const urls = [
+        {
+            breadcrumb: 'makeup_body',
+            link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146943',
+        },
+        // {
+        //     breadcrumb: 'makeup_face',
+        //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146955',
+        // },
+        // {
+        //     breadcrumb: 'makeup_lips',
+        //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146989',
+        // },
+        // {
+        //     breadcrumb: 'makeup_palettes',
+        //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637147002',
+        // },
+        // {
+        //     breadcrumb: 'makeup_eye',
+        //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146947',
+        // },
+
+        // {
+        //     breadcrumb: 'skincare_cleanser-exfoliator',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147114',
+        // },
+        // {
+        //     breadcrumb: 'skincare_eye-care',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147125',
+        // },
+        // {
+        //     breadcrumb: 'skincare_lips-treatment',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147132',
+        // },
+        // {
+        //     breadcrumb: 'skincare_mask-blackhead',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147136',
+        // },
+        // {
+        //     breadcrumb: 'skincare_moisturizers',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147143',
+        // },
+        // {
+        //     breadcrumb: 'skincare_suncare',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147152',
+        // },
+        // {
+        //     breadcrumb: 'skincare_treatments',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147155',
+        // },
+        // {
+        //     breadcrumb: 'skincare_makeup-remove',
+        //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637160349',
+        // },
+        // {
+        //     breadcrumb: 'fragrance_home',
+        //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637146896',
+        // },
+        // {
+        //     breadcrumb: 'fragrance_men',
+        //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637146899',
+        // },
+        // {
+        //     breadcrumb: 'fragrance_women',
+        //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637146906',
+        // },
+        // {
+        //     breadcrumb: 'fragrance_unisex',
+        //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637160328',
+        // },
+    ]
     try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            // args: ['--start-fullscreen'],
-        })
-        const page = await browser.newPage()
-        await page.setViewport({ width: 0, height: 0 })
-
-        const urls = [
-            {
-                breadcrumb: 'makeup_body',
-                link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146943',
-            },
-            // {
-            //     breadcrumb: 'makeup_face',
-            //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146955',
-            // },
-            // {
-            //     breadcrumb: 'makeup_lips',
-            //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146989',
-            // },
-            // {
-            //     breadcrumb: 'makeup_palettes',
-            //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637147002',
-            // },
-            // {
-            //     breadcrumb: 'makeup_eye',
-            //     link: 'https://www.eveandboy.com/product?category=5637146942&sub=5637146947',
-            // },
-
-            // {
-            //     breadcrumb: 'skincare_cleanser-exfoliator',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147114',
-            // },
-            // {
-            //     breadcrumb: 'skincare_eye-care',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147125',
-            // },
-            // {
-            //     breadcrumb: 'skincare_lips-treatment',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147132',
-            // },
-            // {
-            //     breadcrumb: 'skincare_mask-blackhead',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147136',
-            // },
-            // {
-            //     breadcrumb: 'skincare_moisturizers',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147143',
-            // },
-            // {
-            //     breadcrumb: 'skincare_suncare',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147152',
-            // },
-            // {
-            //     breadcrumb: 'skincare_treatments',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637147155',
-            // },
-            // {
-            //     breadcrumb: 'skincare_makeup-remove',
-            //     link: 'https://www.eveandboy.com/product?category=5637147103&sub=5637160349',
-            // },
-            // {
-            //     breadcrumb: 'fragrance_home',
-            //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637146896',
-            // },
-            // {
-            //     breadcrumb: 'fragrance_men',
-            //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637146899',
-            // },
-            // {
-            //     breadcrumb: 'fragrance_women',
-            //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637146906',
-            // },
-            // {
-            //     breadcrumb: 'fragrance_unisex',
-            //     link: 'https://www.eveandboy.com/product?category=5637146890&sub=5637160328',
-            // },
-        ]
-
         for (let i = 0; i < urls.length; i++) {
             const item = urls[i]
             await page.goto(`${item.link}`, { waitUntil: 'networkidle2' })
@@ -155,8 +154,10 @@ export const run = async () => {
             // end csv
             stream.end()
         }
-        await browser.close()
     } catch (e) {
         console.log(e)
+    } finally {
+        await page.close()
+        await browser.close()
     }
 }
